9e6b955535eb56f1c4ced5c186052ad9bc67daaf
XCorde <XCorde1739@icloud.com>
2017-03-26 14:31:27 +0800

    Created Main File
    Created Main code and GUI code

---
+++ bin/GUI.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.awt.*;
+import java.util.*;
+import java.lang.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class GUI {
+	
+}
---
+++ bin/Main.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.awt.*;
+import java.util.*;
+import java.lang.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
